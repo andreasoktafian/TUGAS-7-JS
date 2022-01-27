8ed2034444adcad0747f216f39b8dd3e81229420
andreas <andreas.oktafiann@gmail.com>
2022-01-27 13:24:44 +0700

    TUGAS JS 7

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+var tinggi = ["150cm", "155cm", "160cm", "165cm", "170cm", "175cm", "180cm", "185cm", "190cm", "195cm"];
+
+for (let i = 0; i < tinggi.length; i++) {
+  console.log("Tinggi badan ", tinggi[i]);
+}
+
+console.log("");
+
+for (let x of tinggi) {
+  console.log("Tinggi badan ", x);
+}
